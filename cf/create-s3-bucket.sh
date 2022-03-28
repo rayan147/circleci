c1825a86b9133dfe876fd72dd0acb27ad9c3a862
Rayan Ramirez <rayan361@gmail.com>
2022-03-28 13:21:45 -0400

    fixed env var name

--- cf/create-s3-bucket.sh
+++ cf/create-s3-bucket.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 # Echo the vars 
 echo $S3_TEMPLATE_POLICY_NAME
-echo $S3_TEMPLATE_BUCKETS_NAME
+echo $S3_TEMPLATE_BUCKET_NAME
 echo $IAM_TEMPLATE_NAME
 
 
 
-aws cloudformation create-stack --stack-name ${IAM_TEMPLATE_NAME} --template-body file://${IAM_TEMPLATE_NAME}.yaml --capabilities CAPABILITY_NAMED_IAM
-aws cloudformation create-stack --stack-name ${S3_TEMPLATE_BUCKETS_NAME} --template-body file://${S3_TEMPLATE_BUCKETS_NAME}.yaml
-aws cloudformation create-stack --stack-name ${S3_TEMPLATE_POLICY_NAME} --template-body file://${S3_TEMPLATE_POLICY_NAME}.yaml 
+aws cloudformation create-stack --stack-name $IAM_TEMPLATE_NAME --template-body file://$IAM_TEMPLATE_NAME.yaml --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation create-stack --stack-name $S3_TEMPLATE_BUCKETS_NAME --template-body file://$S3_TEMPLATE_BUCKETS_NAME.yaml
+aws cloudformation create-stack --stack-name $S3_TEMPLATE_POLICY_NAME --template-body file://$S3_TEMPLATE_POLICY_NAME.yaml 
