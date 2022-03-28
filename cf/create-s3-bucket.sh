4069905b35ad9e472377777e0f8744bd0d6137c6
Rayan Ramirez <rayan361@gmail.com>
2022-03-28 12:52:12 -0400

    added code as infrastrure as a job

---
+++ cf/create-s3-bucket.sh
@@ -0,0 +1,11 @@
+S3_TEMPLATE_POLICY_NAME=${S3_TEMPLATE_NAME:?"S3_TEMPLATE_NAME is not set"}
+S3_TEMPLATE_BUCKETS_NAME=${S3_TEMPLATE_BUCKETS_NAME:?"S3_TEMPLATE_BUCKETS_NAME is not set"}
+IAM_TEMPLATE_NAME=${IAM_TEMPLATE_NAME:?"IAM_TEMPLATE_NAME is not set"}
+
+
+
+
+
+aws cloudformation create-stack --stack-name ${IAM_TEMPLATE_NAME} --template-body file://${IAM_TEMPLATE_NAME}.yaml --capabilities CAPABILITY_NAMED_IAM
+aws cloudformation create-stack --stack-name ${S3_TEMPLATE_BUCKETS_NAME} --template-body file://${S3_TEMPLATE_BUCKETS_NAME}.yaml
+aws cloudformation create-stack --stack-name ${S3_TEMPLATE_POLICY_NAME} --template-body file://${S3_TEMPLATE_POLICY_NAME}.yaml 
